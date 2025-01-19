b97d2aacda8259f18be968a3425902c8a1d01ede
Real-zero-cool <bsmithft@gmail.com>
2025-01-19 15:56:11 -0600

    Update V42.js

--- assets/js/V42.js
+++ assets/js/V42.js
@@ -408,7 +408,7 @@ function fetchAndRenderChart(videoID) {
         xaxis: {
           categories: xAxisCategories,
           title: {
-            text: 'Range Count'
+            text: 'Input Points'
           }
         },
         yaxis: {
